1446258eae009d7470c0ffa085318403ba70ee60
LuckHost <sh76484@gmail.com>
2024-02-22 22:41:52 +0700

    A quite little of pep8 in java

--- 2nd/src/Student.java
+++ 2nd/src/Student.java
@@ -1,65 +1,86 @@
 public class Student {
-    private String name = "No name";
-    private String fieldOfStudy = "Not specified";
-    private Float averageRating = 0f;
-    private Integer yearOfAdmission = 2023;
+  private String name = "No name";
+  private String fieldOfStudy = "Not specified";
+  private Float averageRating = 0f;
+  private Integer yearOfAdmission = 2023;
 
-    /*
-     * Ниже находятся функции
-     * геттеры и сеттеры для каждого
-     * из аргументов класса
-     */
-    public void setName(String newName) {
-        name = newName;
-    }
+  /*
+   * Ниже находятся функции
+   * геттеры и сеттеры для каждого
+   * из аргументов класса
+   */
+  public String setName(String newName) {
+		if(newName.matches("\\d+")) {
+			return "The string contains numbers, the name is not set";
+		}
+    name = newName;
+		return "The name has been successfully set";
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setStudyField(String newStudyField) {
-        fieldOfStudy = newStudyField;
-    }
-    
-    public String getStudyField() {
-        return fieldOfStudy;
-    }
+  public String setStudyField(String newStudyField) {
+		if(newStudyField.matches("\\d+")) {
+			return "The string contains numbers, the study field is not set";
+		}
+    fieldOfStudy = newStudyField;
+		return "The study field has been successfully set";
+  }
 
-    public void setAverageRating(float newAverageRating) {
-        averageRating = newAverageRating;
-    }
-    
-    public Float getAverageRating() {
-        return averageRating;
-    }
+  public String getStudyField() {
+    return fieldOfStudy;
+  }
 
-    public void setYearOfAdmission(Integer newYearOfAdmission) {
-        yearOfAdmission = newYearOfAdmission;
-    }
-    
-    public Integer getYearOfAdmission() {
-        return yearOfAdmission;
-    }
+  public String setAverageRating(float newAverageRating) {
+		// средняя оценка не может быть больше 5 и меньше 1
+    if(newAverageRating > 5 
+		|| newAverageRating < 1) {
+			return "The number is not between 1 and 5, the average rating is not set";
+		}
+    averageRating = newAverageRating;
+		return "The averge rating field has been successfully set";
+  }
 
-    /*
-     * Та самая "Уникальная" функция
-     * Высчитывает курс, на которм находится студент
-     */
-    public Integer getCourse() {
-        return 2024 - yearOfAdmission;
-    }
+  public Float getAverageRating() {
+    return averageRating;
+  }
 
-    /*
-     * Конструктор класса
-     */
-    public Student(String newName, String newStudyField, 
-    float newAverageRating, Integer newYearOfAdmission){
-        setName(newName);
-        setStudyField(newStudyField);
-        setAverageRating(newAverageRating);
-        setYearOfAdmission(newYearOfAdmission);
-    }
+  public String setYearOfAdmission(Integer newYearOfAdmission) {
+		// максимальный срок обуения - 11 лет
+    // значит и минимальный год поступления - 2013
+		if(newYearOfAdmission > 2023 
+		|| newYearOfAdmission < 2013) {
+			return "The year is not included in the range of 2013 and 2023 years, the admission year field is not set";
+		}
+    yearOfAdmission = newYearOfAdmission;
+		return "The averge year of admission field has been successfully set";
+  }
 
-    public Student() {
-    }
+  public Integer getYearOfAdmission() {
+    return yearOfAdmission;
+  }
+
+  /*
+   * Та самая "Уникальная" функция
+   * Высчитывает курс, на которм находится студент
+   */
+  public Integer getCourse() {
+    return 2024 - yearOfAdmission;
+  }
+
+  /*
+   * Конструктор класса
+   */
+  public Student(String newName, String newStudyField,
+  float newAverageRating, Integer newYearOfAdmission) {
+    System.out.println(setName(newName));
+    System.out.println(setStudyField(newStudyField));
+    System.out.println(setAverageRating(newAverageRating));
+    System.out.println(setYearOfAdmission(newYearOfAdmission));
+  }
+
+  public Student() {
+  }
 }
