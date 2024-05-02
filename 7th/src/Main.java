c2691c31888d062f9df7dc645440d763658555e6
LuckHost <sh76484@gmail.com>
2024-05-02 15:15:42 +0700

    init 7th project commit

---
+++ 7th/src/Main.java
@@ -0,0 +1,427 @@
+/* 
+ * Выполнил Ходыкин Александр КИ23-17/2Б
+ * Ссылка на удаленный репозиторий:
+ * https://github.com/LuckHost/OP_sfu_progDev_1st_course_2semester
+ * 
+ * Номер варианта: 7
+ * 
+ * Задание:
+ * Входные данные: Необходимо реализовать класс "Студент", 
+ * в том числе придумать поля (атрибуты) для него: 
+ * числовые (целое и вещественное, не менее двух) и текстовые (не менее двух). 
+ * Все поля класса должны быть закрытыми, необходимо реализовать методы доступа. 
+ * В классе должны быть реализованы конструктор по умолчанию и с параметрами.
+*/
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+  /*
+   * Главная функция программы
+   * 
+   * Приветствует пользователя, потом запускает цикл программы
+   * 
+   */
+  public static void main(String[] args) throws Throwable {
+    System.out.println("Hello!\n" +
+    "The student information preservation " +
+    "program program welcomes you\n");
+    ArrayList<Student> studentsBase = new ArrayList<Student>();
+    boolean working = true;
+    while (working) {
+      Integer taskSelected = getCorrectInput(0, 14, 
+        new String("Enter the task:\n" +
+        "0 - Add new student with empty values\n" +
+        "1 - Add new student and enter the values\n" +
+        "2 - Edit student values by index\n" +
+        "3 - Print all students to the console\n" + 
+        "4 - Sort the list by field\n" + 
+        "5 - createStreamFromList\n" + 
+        "6 - filterByCriteria\n" + 
+        "7 - removeDuplicates\n" + 
+        "8 - accumulateOperation\n" + 
+        "9 - demonstrateOptional\n" +
+        "10 - groupByFieldAndCount\n" + 
+        "11 - demonstrateSummaryStatistics\n" + 
+        "12 - saveDataToFile\n" + 
+        "13 - loadDataFromFile\n" +  
+        "14 - exit"));
+      switch (taskSelected) {
+        case 0:
+          Student newStudent = new Student();
+          studentsBase.add(newStudent);
+          break;
+        case 1:
+          String name = getCorrectStrInput(new String(
+            "Enter the student's name: "));
+          String fieldOfStudy = getCorrectStrInput(new String(
+            "Enter the student's field of study: "));
+          Integer yearOfAdmission = getCorrectInput(new String(
+            "Enter the student's year of admission: "));
+
+          // Обработчик исключения InvalidIntInputException,
+          // которое было повторно создано в Students.java
+          try {
+            newStudent = new Student(name, fieldOfStudy, yearOfAdmission);
+            studentsBase.add(newStudent);
+          } catch (InvalidIntInputException e) {
+            System.out.println(e.getMessage());
+          }
+          break;
+        case 2:
+          if(studentsBase.size() == 0) {
+            System.out.println(new String("There is no students to change now "));
+            break;
+          }
+          printAllStudents(studentsBase);
+          changeStudentsField(studentsBase);
+          break;
+        case 3:
+          if(studentsBase.size() == 0) {
+            System.out.println(new String("There is no students to print now "));
+            break;
+          }
+          printAllStudents(studentsBase);
+          break;
+        case 4:
+          if(studentsBase.size() == 0) {
+            System.out.println(new String("There is no students to sort now "));
+            break;
+          }
+          sortStudentBase(studentsBase);
+          break;
+        case 5:
+          createStreamFromList(studentsBase);
+          break;
+        case 6:
+          filterByCriteria(studentsBase);
+          break;
+        case 7:
+          removeDuplicates(studentsBase);
+          break;
+        case 8:
+          accumulateOperation(studentsBase);
+          break;
+        case 9:
+          demonstrateOptional();
+          break;
+        case 10:
+          groupByFieldAndCount(studentsBase);
+          break;
+        case 11:
+          demonstrateSummaryStatistics(studentsBase);
+          break;
+        case 12:
+          saveDataToFile(studentsBase);
+          break;
+        case 13:
+          loadDataFromFile();
+          break;
+        case 14:
+          working = false;
+          break;
+        default:
+          // getCorrectInput не позволит добраться сюда
+          break;
+      }
+    }
+  }
+
+  /*
+   * Выводит в консоль всех студентов
+   * 
+   * Принимает базу студентов 
+   */
+  public static void printAllStudents(ArrayList<Student> studentsBase) {
+    int counter = 0;
+    for (Student student : studentsBase) {
+      System.out.println(counter + " | " + student.getName() + " " 
+      + student.getStudyField() + " "
+      + student.getAverageRating() + " " 
+      + student.getYearOfAdmission() + " " 
+      + student.getCourse());
+      counter++;
+    }
+    
+  }
+
+  /*
+   * Сортирует массив студентов по любому полю
+   * 
+   * Принимает массив студентов
+   * 
+   * Источник "вдохновения":
+   * https://stackoverflow.com/questions/15452429/java-arrays-sort-2d-array
+   * 
+   */
+  public static void sortStudentBase(ArrayList<Student> studentsBase) {
+    Integer fieldSelected = getCorrectInput(0, 4, 
+      new String("Enter the student's field you want to change:\n" +
+      "0 - Name\n" +
+      "1 - Field of study\n" +
+      "2 - Average rating\n" +
+      "3 - Year of admission\n" +
+      "4 - Course\n"));
+    switch (fieldSelected) {
+      case 0:
+        Collections.sort(studentsBase, new Comparator<Student>() {
+          public int compare(Student o1, Student o2) {
+            return - o2.getName().compareTo(o1.getName());
+            }
+          });
+        break;
+      case 1:
+        Collections.sort(studentsBase, new Comparator<Student>() {
+          public int compare(Student o1, Student o2) {
+            return - o2.getStudyField().compareTo(o1.getStudyField());
+            }
+          });
+        break;
+      case 2:
+        Collections.sort(studentsBase, new Comparator<Student>() {
+          public int compare(Student o1, Student o2) {
+            return - o2.getAverageRating().compareTo(o1.getAverageRating());
+            }
+        });
+        break;
+      case 3:
+        Collections.sort(studentsBase, new Comparator<Student>() {
+          public int compare(Student o1, Student o2) {
+            return - o2.getYearOfAdmission().compareTo(o1.getYearOfAdmission());
+            }
+          });
+        break;
+      case 4:
+        Collections.sort(studentsBase, new Comparator<Student>() {
+          public int compare(Student o1, Student o2) {
+            return - o2.getCourse().compareTo(o1.getCourse());
+            }
+          });
+        break;
+      default:
+        // getCorrectInput не позволит добраться сюда
+        break;
+    }
+  }
+
+  /*
+   * Изменяет любое данное поле
+   * любого студента в базе
+   * Принимает базу студентов
+   */
+  public static void changeStudentsField(ArrayList<Student> studentsBase) throws 
+  InvalidStringInputException, InvalidIntInputException {
+    Integer userId = getCorrectInput(0, 3, 
+      new String("Enter the student's number you want to change: "));
+    Student stdToChange = studentsBase.get(userId);
+    Integer fieldSelected = getCorrectInput(0, 5, 
+      new String("Enter the student's field you want to change:\n" +
+      "0 - Name\n" +
+      "1 - Field of study\n" +
+      "2 - Average rating\n" +
+      "3 - Year of admission\n"));
+    switch (fieldSelected) {
+      case 0:
+        String name = getCorrectStrInput(new String(
+          "Enter the student's name: "));
+        System.out.println(stdToChange.setName(name));
+        break;
+      case 1:
+        String fieldOfStudy = getCorrectStrInput(new String(
+          "Enter the student's field of study: "));
+        System.out.println(stdToChange.setStudyField(fieldOfStudy));
+        break;
+      case 2:
+        float averageRating = getCorrectFloatInput(new String(
+          "Enter the student's average rating: "));
+        System.out.println(stdToChange.setAverageRating(averageRating));
+        break;
+      case 3:
+        Integer yearOfAdmission = getCorrectInput(2013, 2023, 
+        new String("Enter the student's year of admission: "));
+        System.out.println(stdToChange.setYearOfAdmission(yearOfAdmission));
+        break;
+      default:
+        // getCorrectInput не позволит добраться сюда
+        break;
+    }
+  }
+
+// Метод для создания потока из списка объектов и вывода их на экран
+  public static void createStreamFromList(ArrayList<Student> studentsBase) {
+    Stream<Student> studentStream = studentsBase.stream();
+    studentStream.forEach(System.out::println);
+  }
+
+  // Метод для фильтрации объектов по заданному признаку, введенному пользователем
+  public static void filterByCriteria(ArrayList<Student> studentsBase) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the criteria to filter by:");
+    float criteria = scanner.nextFloat();
+    List<Student> filteredStudents = studentsBase.stream()
+        .filter(student -> student.getAverageRating() > criteria)
+        .collect(Collectors.toList());
+    filteredStudents.forEach(System.out::println);
+  }
+
+  // Метод для удаления дубликатов из списка
+  public static void removeDuplicates(ArrayList<Student> studentsBase) {
+    List<Student> uniqueStudents = studentsBase.stream()
+        .distinct()
+        .collect(Collectors.toList());
+    uniqueStudents.forEach(System.out::println);
+  }
+
+  // Метод для демонстрации операции сведения с накоплением (например, сумма всех элементов)
+  public static void accumulateOperation(ArrayList<Student> studentsBase) {
+    float sum = studentsBase.stream()
+          .mapToInt(student -> student.getYearOfAdmission())
+          .sum();
+    System.out.println("Sum of all average ratings: " + sum);
+  }
+
+  // Метод для демонстрации работы с типом Optional
+  public static void demonstrateOptional() {
+    Optional<String> optionalString = Optional.ofNullable(null);
+    if (optionalString.isPresent()) {
+      System.out.println("Value: " + optionalString.get());
+    } else {
+      System.out.println("Value is not present");
+    }
+  }
+
+  // Метод для группировки объектов по некоторому полю и вывода числа элементов в группе
+  public static void groupByFieldAndCount(ArrayList<Student> studentsBase) {
+    long countByFieldOfStudy = studentsBase.stream()
+        .collect(Collectors.groupingBy(Student::getStudyField, Collectors.counting()))
+        .entrySet().stream()
+        .peek(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()))
+        .count();
+    System.out.println("Total groups: " + countByFieldOfStudy);
+  }
+
+  // Метод для демонстрации работы с SummaryStatistics
+  public static void demonstrateSummaryStatistics(ArrayList<Student> studentsBase) {
+    double averageRatingAverage = studentsBase.stream()
+        .mapToDouble(Student::getAverageRating)
+        .summaryStatistics()
+        .getAverage();
+    System.out.println("Average average rating: " + averageRatingAverage);
+  }
+
+  // Метод для загрузки данных из файла
+  public static void loadDataFromFile() {
+      // Реализация загрузки данных из файла
+  }
+
+  // Метод для сохранения данных в файл
+  public static void saveDataToFile(ArrayList<Student> studentsBase) {
+      // Реализация сохранения данных в файл
+  }
+
+  /*
+   * Функция импорта корректного строкового значения
+   * 
+   * Принимает сообщение, которое надо выдать пользователю
+   * 
+   * Возвращает строку без спец. символов 
+   */
+  public static String getCorrectStrInput(String message) {
+    System.out.println(message);
+    Scanner scanner = new Scanner(System.in);
+    String output = null;
+    String[] wrongValues = new String[]{"@", "#", "$", "%", "^", "&", "*", "+", "="};
+    Boolean cntnue = true;
+    while (cntnue) {
+      if (scanner.hasNextLine()) output = scanner.nextLine();
+      else continue;
+      
+      for (String wrongValue : wrongValues) {
+        if(output.indexOf(wrongValue) != -1){
+          System.out.println("The input has forbidden characters. Please try again");
+          continue;
+        } else {
+          cntnue = false;
+          break;
+        }
+      }
+    }
+    return output;
+  }
+
+  /*
+   * Функция импорта корректного значения типа float
+   * 
+   * Принимает сообщение, которое надо выдать пользователю
+   * 
+   * Возвращает значение float 
+   */
+  public static float getCorrectFloatInput(String message) {
+    System.out.println(message);
+    Scanner scanner = new Scanner(System.in);
+    while (true) {
+      if(scanner.hasNextFloat()) {
+        float result = scanner.nextFloat();
+        if(result >= 0 && result <= 5) {
+          return result;
+        }
+        System.out.println("The input is incorrect. Please try again");
+        scanner = new Scanner(System.in);
+      } else {
+        scanner = new Scanner(System.in);
+        System.out.println("Try again");
+      }
+    }
+  }
+
+  /*
+  * Функция перегрузки для функции ввода переменных
+  * 
+  * Принимет сообщение, которое надо отправить
+  * пользователю перед вводом данных
+  * 
+  * Возвращает значение функции ввода, в котрую
+  * отправляет сообщение и пустой массив
+  * 
+  */
+  public static int getCorrectInput(String message) throws Exception {
+    return getCorrectInput(0, 0, message);
+  }
+
+  /*
+  * Функция корректного ввода
+  * Просит пользователя ввести значение
+  * до тех пор, пока оно не будет корректным 
+  * 
+  * Получет на вход область допустимых
+  * значений в виде начала и конца массива 
+  * и строку сообщения
+  * для пользователя
+  * 
+  * Возвращает число, введенное пользователем
+  */
+  public static int getCorrectInput(int crctVlsStart, int crctVlsEnd, String message) {
+    System.out.println(message);
+    Scanner scanner = new Scanner(System.in);
+    while (true) {
+      scanner = new Scanner(System.in);
+      if(scanner.hasNextInt()) {
+        int result = Integer.parseInt(scanner.nextLine());
+        if((result >= crctVlsStart && result <= crctVlsEnd) || crctVlsStart == crctVlsEnd) {
+          return result;
+        }
+        System.out.println("The input is incorrect. Please try again");
+        
+      } else {
+        System.out.println("Try again");
+      }
+    }
+  }
+}
---
+++ 7th/src/Student.java
@@ -0,0 +1,144 @@
+import java.util.logging.Logger;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Student {
+  private String name = "No name";
+  private String fieldOfStudy = "Not specified";
+  private Float averageRating = 0f;
+  private Integer yearOfAdmission = 2023;
+
+  /*
+   * Ниже находятся функции
+   * геттеры и сеттеры для каждого
+   * из аргументов класса
+   */
+  public String setName(String newName) throws 
+  InvalidStringInputException {
+    if(newName.matches(".*\\d.*")) {
+      throw new InvalidStringInputException(
+        "The name string must not have numbers");
+    }
+    name = newName;
+    return "The name has been successfully set";
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String setStudyField(String newStudyField) throws 
+  InvalidStringInputException {
+    if(newStudyField.matches(".*\\d.*")) {
+      throw new InvalidStringInputException(
+        "The study field must not have numbers");
+    }
+    fieldOfStudy = newStudyField;
+    return "The study field has been successfully set";
+  }
+
+  public String getStudyField() {
+    return fieldOfStudy;
+  }
+
+  public String setAverageRating(float newAverageRating) throws 
+  InvalidIntInputException {
+    // средняя оценка не может быть больше 5 и меньше 1
+    if(newAverageRating > 5 
+    || newAverageRating < 1) {
+      throw new InvalidIntInputException("The number is not between 1 and 5, "+
+      "the average rating is not set");
+    }
+    averageRating = newAverageRating;
+    return "The average rating field has been successfully set";
+  }
+
+  public Float getAverageRating() {
+    return averageRating;
+  }
+
+  public String setYearOfAdmission(Integer newYearOfAdmission) throws 
+  InvalidIntInputException {
+    // максимальный срок обуения - 11 лет
+    // значит и минимальный год поступления - 2013
+    if(newYearOfAdmission > 2023 
+    || newYearOfAdmission < 2013) {
+      throw new InvalidIntInputException(
+        "The year is not included in the range of 2013 and 2023 years, " + 
+        "the admission year field is not set");
+    }
+    yearOfAdmission = newYearOfAdmission;
+    return "The year of admission field has been successfully set";
+  }
+
+  public Integer getYearOfAdmission() {
+    return yearOfAdmission;
+  }
+
+  /*
+   * Та самая "Уникальная" функция
+   * Высчитывает курс, на которм находится студент
+   */
+  public Integer getCourse() {
+    return 2024 - yearOfAdmission;
+  }
+
+  /*
+   * Конструктор класса
+   * 
+   * Перехватывает 
+   * InvalidStringInputException
+   * и
+   * InvalidIntInputException
+   * 
+   */
+  public Student(String newName, String newStudyField, 
+  Integer newYearOfAdmission) throws InvalidIntInputException, 
+  InputMismatchException, InvalidStringInputException, Exception {
+    Scanner scanner = null;
+    float result = 0f;
+    try {
+      // Для примера подавленного исключения, а также 
+      // для применения "утверждения" создан импорт числа здесь
+      System.out.println("Enter the student's average rating: ");
+      scanner = new Scanner(System.in);
+      result = scanner.nextFloat();
+      assert result > 0;
+
+      System.out.println(setName(newName));
+      System.out.println(setStudyField(newStudyField));
+      System.out.println(setYearOfAdmission(newYearOfAdmission));
+    } catch (InvalidStringInputException e) {
+      // Простой перехват собственного исключения
+      // Здесь новому студенту, в зависимости от ошибки,
+      // присваивается "No name" либо "Not specified",
+      // после чего он успешно добавляется в массив, 
+      // если остальные поля корректны
+      System.out.println(e.getMessage());
+    } catch (InvalidIntInputException e) {
+      // Повторное генерирование, обрабатывается в Main.java
+      // Новый студент не создается
+      Exception se = new InvalidIntInputException(e.getMessage());
+      se.initCause(e);
+      throw se;
+    } catch (InputMismatchException e) {
+      // Простой перехват базовго исключения
+      System.out.println("An error occurred when entering the value.");
+    } finally {
+      // подавленное исключение 
+      try {
+        System.out.println(setAverageRating(result));
+      } catch (Exception e) {
+        System.out.println("Failed to create a new student. " + 
+        "Check the entered data.");
+        // пример логгирования
+        Logger.getGlobal().info("A new suppressed exception has been created");
+        Exception se = new InvalidIntInputException(e.getMessage());
+        se.initCause(e);
+        throw se;
+      }
+    }
+  }
+  public Student() {
+  }
+}
